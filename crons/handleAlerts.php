825ee7fc088c64f9d18abcd1b2ab92641e5fedf5
Bojan Seirovski <druid0101@gmail.com>
2017-04-07 15:54:37 -0400

    PHP mailer added, alrets cron updated
    The Alerts cron sends emails to all of the registered users

--- crons/handleAlerts.php
+++ crons/handleAlerts.php
@@ -1,39 +1,65 @@
 <?php
-include	'funct.php';
-$dsn	=	require	dirname(__FILE__)	.	DIRECTORY_SEPARATOR. '..'.	DIRECTORY_SEPARATOR. 'smarthome'.	DIRECTORY_SEPARATOR. 'protected'.	DIRECTORY_SEPARATOR. 'config'.	DIRECTORY_SEPARATOR. 'database.php';
+include 'funct.php';
+include './phpmailer/PHPMailerAutoload.php';
 
-$con=new \PDO($dsn['connectionString'], $dsn['username'], $dsn['password']);
+$mailSettings = [
+    'host'=>'smtp.gmail.com',
+    'port'=>587,
+    'username'=>"cablescalc@gmail.com",
+    'password'=>"rim%@!641",
+    'name'=>"SensiHome Alert",
+    'subject'=>'SensiHome Alert - automated action',
+];
+
+$dsn = array(
+    'connectionString' => 'mysql:host=localhost;dbname=smarthome',
+    'emulatePrepare' => true,
+    'username' => 'root',
+    'password' => 'rim%@!641',
+    'charset' => 'utf8',
+);
+
+$con = new \PDO($dsn['connectionString'], $dsn['username'], $dsn['password']);
+
 
 /**
-	* Use PDO to read the DB and curl to talk to
-	* the devices
-	* alert table
-	* 
-	*/
-$sData	=	runQuery($con,	'SELECT * FROM alert;',	null,true);
+ * Use PDO to read the DB and curl to talk to
+ * the devices
+ * alert table
+ * 
+ */
+$users = runQuery($con, 'SELECT username, name FROM user;', null, true);
+$sData = runQuery($con, 'SELECT * FROM alert;', null, true);
 
-foreach($sData as $oneModule){
-				$oneSensorData	=	runQuery($con,	'SELECT id, serial,com_id FROM sensor WHERE id=:tby;',	array(':tby'=>$oneModule['triggered_by']),true);
-				if(!isset($oneSensorData[0])){
-								continue;
-				}
-			 $requestEndPoint = 'http://'.$oneSensorData[0]['com_id']. '/id/'.$oneSensorData[0]['serial'].'/reqtype/json';
-				
-				$measure = getSimpleRequest($requestEndPoint);
-				
-				if(isset($measure) && isset($measure['status']) && ($measure['status']=='OK')){
-								if(isset($oneModule['value']) && ($oneModule['trigger_value']>=$measure['value'])){
-												try{
-																// load actuator values
-																$requestEndPoint = 'http://'.$oneModule['actuator_id']. '/id/'.$oneModule['serial'].'/reqtype/json/pin/ON';
-																$fireAlert = getSimpleRequest($requestEndPoint);
-																if($fireAlert['status']!='OK'){
-																				error_log('Actuator ID '.$oneModule['actuator_id']." wasn't set.");
-																}																
-												}
-												catch(Exception $e){
-																error_log($e->getMessage());
-												}
-								}
-				}
+foreach ($sData as $oneModule) {
+    $oneSensorData = runQuery($con, 'SELECT id, serial,com_id, name FROM sensor WHERE id=:tby;', array(':tby' => $oneModule['triggered_by']), true);
+    $oneActData = runQuery($con, 'SELECT id, serial,com_id, name FROM actuator WHERE id=:aid;', array(':aid' => $oneModule['actuator_id']), true);
+    if (!isset($oneSensorData[0])) {
+	continue;
+    }
+    $requestEndPoint = 'http://' . $oneSensorData[0]['com_id'] . '/id/' . $oneSensorData[0]['serial'] . '/reqtype/json';
+
+    $measure = getSimpleRequest($requestEndPoint);
+
+    if (isset($measure) && isset($measure['status']) && ($measure['status'] == 'OK')) {
+	if (isset($oneModule['value']) && ($oneModule['trigger_value'] >= $measure['value'])) {
+	    try {
+		// load actuator values
+		$requestEndPoint = 'http://' . $oneModule['actuator_id'] . '/id/' . $oneModule['serial'] . '/reqtype/json/pin/ON';
+		$fireAlert = getSimpleRequest($requestEndPoint);
+		if ($fireAlert['status'] != 'OK') {
+		    error_log('Actuator ID ' . $oneModule['actuator_id'] . " wasn't set.");
+		}
+		else{
+		    foreach($users as $oneUser){
+			$content = 'Actuator ' . $oneActData['name'] . " was activated by sensor ".$oneSensorData['name'].".";
+			sendMail($mailSettings, $oneUser, $content);
+		    }
+		}
+	    }
+	    catch (Exception $e) {
+		error_log($e->getMessage());
+	    }
+	}
+    }
 }
